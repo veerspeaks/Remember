e7cc2727d4f1824f2e53566023174a1c87471a1d
Swapnabir Dutta <accibc@gmail.com>
2024-10-22 16:19:30 +0530

    implemented web hosting of mySQL

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,28 +1,26 @@
-import mysql from 'mysql2/promise'
-import { configDotenv } from 'dotenv'
+import mysql from 'mysql2/promise';
 
 async function query(sql, params) {
     try {
         const connection = await mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "GfYa7084@",
-            database: "flashcard_db"
+            host: "sql12.freemysqlhosting.net", // new host
+            user: "sql12739815", // new username
+            password: "caYUSUFEW5", // new password
+            database: "sql12739815", // database name
+            port: 3306 // use the given port
         });
         
-        if(connection){
-            console.log("Connection established")
+        if (connection) {
+            console.log("Connection established");
         }
 
         const [results] = await connection.execute(sql, params);
         connection.end(); // Close the connection after executing the query
         return results;
-        
+
     } catch (e) {
-        console.log("couldn't establish connection", e);
+        console.log("Couldn't establish connection", e);
     }
 }
 
-
 export default query;
-
