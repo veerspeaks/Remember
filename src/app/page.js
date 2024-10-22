3625b8f3e619538c15914401622668bf35e31b24
Swapnabir Dutta <accibc@gmail.com>
2024-10-22 19:50:18 +0530

    added the url of render on other pages as well

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,9 @@ import CategoryList from "@/components/CategoryList";
 
 // Fetch categories server-side
 export default async function Home() {
-  const res = await fetch('https://remember-4wzs.onrender.com/api/categories', { next: { revalidate: 10 } });
+  const res = await fetch('https://remember-4wzs.onrender.com/api/categories', {
+    cache: 'no-store'  // Or use 'force-cache' if you want to cache
+  });
   const categories = await res.json();
 
   return (
