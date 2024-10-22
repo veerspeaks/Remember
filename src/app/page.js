dada27c6e04cfdc21730479a17533b62fd198cb8
Swapnabir Dutta <accibc@gmail.com>
2024-10-22 19:25:49 +0530

    changed to server side data fetching for the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import CategoryList from "@/components/CategoryList";
 
+// Fetch categories server-side
 export default async function Home() {
-  
+  const res = await fetch('http://localhost:3000/api/categories', {
+    cache: 'no-store'  // Or use 'force-cache' if you want to cache
+  });
+  const categories = await res.json();
+
   return (
     <div className="flex justify-center">
-      <CategoryList />
+      {/* Pass the fetched categories as a prop */}
+      <CategoryList categories={categories} />
     </div>
   );
 }
