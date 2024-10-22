05294a93797bf834fdfbb0624801dbb95ce61e39
Swapnabir Dutta <accibc@gmail.com>
2024-10-22 19:43:11 +0530

    add the url of render

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import CategoryList from "@/components/CategoryList";
 
 // Fetch categories server-side
 export default async function Home() {
-  const res = await fetch('http://localhost:3000/api/categories', { next: { revalidate: 10 } });
+  const res = await fetch('https://remember-4wzs.onrender.com/api/categories', { next: { revalidate: 10 } });
   const categories = await res.json();
 
   return (
