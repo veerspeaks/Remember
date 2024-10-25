d16980043545ba6565477d5ceb2ac392e2dfd9d9
Swapnabir Dutta <accibc@gmail.com>
2024-10-25 21:39:18 +0530

    added loading screen

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+// app/loading.js
+
+export default function Loading() {
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
+        <p className="text-lg font-semibold">Loading...</p>
+      </div>
+    );
+  }
+  
