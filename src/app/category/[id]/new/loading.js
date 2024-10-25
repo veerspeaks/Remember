bfb372841e941ab8fab60cd71e79c5ca7b73ed1a
Swapnabir Dutta <accibc@gmail.com>
2024-10-26 00:02:16 +0530

    fixed import error for loading screen

--- src/app/category/[id]/new/loading.js
+++ src/app/category/[id]/new/loading.js
@@ -1,24 +1,10 @@
-import localFont from "next/font/local";
-import "./globals.css";
+import Loading from '@/components/Loading'
+import React from 'react'
 
-const geistMono = localFont({
-  src: "./fonts/Bangers-Regular.ttf",
-  variable: "--font-geist-mono",
-  weight: "400",
-});
-
-export default function Loading() {
+const loading = () => {
   return (
-    <div
-      className={`${geistMono.variable} fixed inset-0 flex items-center justify-center bg-black bg-opacity-100 z-50`}
-    >
-      <h1 className="text-4xl font-bangers font-bold text-red-800 remember-animation">
-        {Array.from("REMEMBER?").map((letter, index) => (
-          <span key={index} className={`bounce delay-${index}`}>
-            {letter}
-          </span>
-        ))}
-      </h1>
-    </div>
-  );
+    <Loading/>
+  )
 }
+
+export default loading
---
+++ src/components/Loading.jsx
@@ -0,0 +1,24 @@
+import localFont from "next/font/local";
+import "../app/globals.css";
+
+const geistMono = localFont({
+  src: "../app/fonts/Bangers-Regular.ttf",
+  variable: "--font-geist-mono",
+  weight: "400",
+});
+
+export default function Loading() {
+  return (
+    <div
+      className={`${geistMono.variable} fixed inset-0 flex items-center justify-center bg-black bg-opacity-100 z-50`}
+    >
+      <h1 className="text-4xl font-bangers font-bold text-red-800 remember-animation">
+        {Array.from("REMEMBER?").map((letter, index) => (
+          <span key={index} className={`bounce delay-${index}`}>
+            {letter}
+          </span>
+        ))}
+      </h1>
+    </div>
+  );
+}
