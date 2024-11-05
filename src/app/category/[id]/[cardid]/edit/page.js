444adedb83d780e5c0f263a2f1d38b9c46328c7f
Swapnabir Dutta <accibc@gmail.com>
2024-11-05 20:05:45 +0530

    changed database address

--- src/app/category/[id]/[cardid]/edit/page.js
+++ src/app/category/[id]/[cardid]/edit/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const EditCardPage = async ({ params }) => {
     // Fetch the data on the server side
-    const response = await axios.get(`https://remember-4wzs.onrender.com/api/singlecard?categoryId=${params.id}&cardId=${params.cardid}`)
+    const response = await axios.get(`https://remember-alpha-silk.vercel.app/api/singlecard?categoryId=${params.id}&cardId=${params.cardid}`)
     const data = response.data[0] // Get the card data
 
     return (
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import CategoryList from "@/components/CategoryList";
 
 // Fetch categories server-side
 export default async function Home() {
-  const res = await fetch('https://remember-4wzs.onrender.com/api/categories', {
+  const res = await fetch('https://remember-alpha-silk.vercel.app/api/categories', {
     cache: 'no-store'  // Or use 'force-cache' if you want to cache
   });
   const categories = await res.json();
